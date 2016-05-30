4c53ee8f280394b5095e981a1349e5fdf049d8a8
Mason Malone <mason@masonm.org>
2016-05-30 19:55:24 -0400

    Comment avsh_config_example.rb properly

--- avsh_config_example.rb
+++ avsh_config_example.rb
@@ -1,4 +1,8 @@
 # Example of the optional config file for AVSH. Copy it to ~/.avsh_config.rb
 # See the "Installation" section of README.md
+
+# Name of the Vagrant VM in which to execute commands.
 AVSH_VM_NAME = 'dev'
+
+# Directory containing the Vagrantfile for the above VM.
 AVSH_VAGRANTFILE_DIR = Dir.pwd
