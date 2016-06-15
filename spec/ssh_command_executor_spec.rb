c200bda2f0695ec0978c59d1adc46677a6a563b5
Mason Malone <mason@masonm.org>
2016-06-14 20:53:34 -0400

    Cleanup for ssh_command_executor_spec.rb

--- spec/ssh_command_executor_spec.rb
+++ spec/ssh_command_executor_spec.rb
@@ -1,42 +1,33 @@
 require 'spec_helper'
 
+shared_examples_for 'a normal command' do |execute_args, ssh_cmd_args|
+  it 'executes succesfully' do
+    expect(Kernel).to receive(:exec).with('ssh', '-o ControlPath /s.sock',
+                                          *ssh_cmd_args)
+    expect { subject.execute(*execute_args) }.to raise_error(Avsh::ExecSshError)
+  end
+end
+
 describe Avsh::SshCommandExecutor do
   subject { described_class.new(double(debug: nil), 'foo_machine', '/s.sock') }
 
   context 'with non-empty command and a guest dir' do
-    it 'changes to guest dir and executes command' do
-      expect(Kernel).to receive(:exec).with('ssh', '-o ControlPath /s.sock',
-                                            '-t', 'foo_machine',
-                                            'cd /somedir; ls')
-      expect { subject.execute('ls', '/somedir') }
-        .to raise_error(Avsh::ExecSshError)
-    end
+    it_behaves_like 'a normal command', ['ls', '/somedir'],
+                    ['-t', 'foo_machine', 'cd /somedir; ls']
   end
 
   context 'with empty command and a guest dir' do
-    it 'changes to guest dir and runs shell' do
-      expect(Kernel).to receive(:exec).with('ssh', '-o ControlPath /s.sock',
-                                            '-t', 'foo_machine',
-                                            'cd /somedir; exec $SHELL -l')
-      expect { subject.execute('', '/somedir') }
-        .to raise_error(Avsh::ExecSshError)
-    end
+    it_behaves_like 'a normal command', ['', '/somedir'],
+                    ['-t', 'foo_machine', 'cd /somedir; exec $SHELL -l']
   end
 
   context 'with non-empty command and no guest dir' do
-    it 'runs command' do
-      expect(Kernel).to receive(:exec).with('ssh', '-o ControlPath /s.sock',
-                                            '-t', 'foo_machine', 'pwd')
-      expect { subject.execute('pwd') }.to raise_error(Avsh::ExecSshError)
-    end
+    it_behaves_like 'a normal command', ['pwd'],
+                    ['-t', 'foo_machine', 'pwd']
   end
 
   context 'with user SSH args, non-empty command and no guest dir' do
-    it 'runs command' do
-      expect(Kernel).to receive(:exec).with('ssh', '-o ControlPath /s.sock',
-                                            '-T', '-6', 'foo_machine', 'pwd')
-      expect { subject.execute('pwd', nil, '-T -6') }
-        .to raise_error(Avsh::ExecSshError)
-    end
+    it_behaves_like 'a normal command', ['pwd', nil, '-T -6'],
+                    ['-T', '-6', 'foo_machine', 'pwd']
   end
 end
